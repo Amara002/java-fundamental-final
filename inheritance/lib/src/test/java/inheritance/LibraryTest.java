81099e8d8df0ebbb3af4eab15458c4715ad5b2e0
Amara002 <amaraalbalkhi@yahoo.com>
2021-07-06 01:21:47 +0300

    work on lab 7&6

--- inheritance/lib/src/test/java/inheritance/LibraryTest.java
+++ inheritance/lib/src/test/java/inheritance/LibraryTest.java
@@ -6,6 +6,7 @@ package inheritance;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import org.junit.*;
+import org.junit.jupiter.api.BeforeEach;
 
 public class LibraryTest {
 
@@ -30,6 +31,14 @@ public class LibraryTest {
         assertEquals("5.5",mac.getCatego());
 
     }
+    @Test
+    public void TestAddReviewRes(){
+        Restaurant mac = new Restaurant("mac",3.0,"5.5");
+        Review RevTest = new Review("IT","Alex",4);
+        mac.addReview(RevTest);
+        assertTrue("retrun true if add review", mac.getReviews().size() == 1);
+
+    }
 
     @Test
     public void testToStringRev() {
@@ -42,7 +51,7 @@ public class LibraryTest {
 
         assertEquals("IT",RevTest.getBody());
         assertEquals("Alex",RevTest.getAuthor());
-        assertEquals(4,RevTest.getStar());
+        assertEquals(4,RevTest.getStar(),1.2);
 
     }
     @Test
@@ -59,11 +68,38 @@ public class LibraryTest {
         assertEquals(5,cityMall.getNumDollar());
 
     }
+
     @Test
     public void testToStringTheater() {
         Theater movie = new Theater("Amara");
         assertEquals("Theater{name='Amara', movies=[], reviews={}}",String.valueOf(movie));
     }
+
+    @Test
+    public void constructorTheater() {
+        Theater movie = new Theater("Amara");
+        assertEquals("Amara",movie.getName());
+
+    }
+
+   @Test
+    public void TestAddReview(){
+        Theater testAdd = new Theater("test");
+       testAdd.addNewRev("test","test",5);
+       assertTrue("retrun true if add review", testAdd.getReviews().size() == 1);
+
+   }
+
+    @Test
+    public void TestAddAndRemoveReview(){
+        Theater testAdd = new Theater("test");
+        testAdd.addMovie("test1");
+        assertTrue("retrun true if add movie", testAdd.getMovies().size() == 1);
+        testAdd.removeMovie("test1");
+        assertTrue("retrun true if remove movie", testAdd.getMovies().size() == 0);
+
+    }
+
 
 
 }
